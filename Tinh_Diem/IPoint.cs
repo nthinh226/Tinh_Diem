e5bd4a3a313ab904fd173d7267ec50a190459d97
Tran Ngoc Thinh <1951065260@e.tlu.edu.vn>
2021-06-04 19:36:47 +0700

    Add project files.

---
+++ Tinh_Diem/IPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tinh_Diem
+{
+    interface IPoint
+    {
+        void Input();
+        void Display();
+    }
+}
---
+++ Tinh_Diem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tinh_Diem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
